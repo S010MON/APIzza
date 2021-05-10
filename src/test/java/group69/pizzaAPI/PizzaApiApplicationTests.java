8cc3db96950026559d53accd0d2537bb29a88c35
S010MON <leon@bdebnath.com>
2021-05-10 15:10:01 +0200

    Intial upload!

---
+++ src/test/java/group69/pizzaAPI/PizzaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package group69.pizzaAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
