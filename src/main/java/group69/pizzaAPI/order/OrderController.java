2631154c82fc3b617c0556836d1c9662111aa922
S010MON <leon@bdebnath.com>
2021-05-23 16:28:35 +0200

    Adds CrossOrigin compatibility

--- src/main/java/group69/pizzaAPI/order/OrderController.java
+++ src/main/java/group69/pizzaAPI/order/OrderController.java
@@ -4,12 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import group69.pizzaAPI.exceptions.OrderNotCancelledException;
@@ -17,6 +12,7 @@ import group69.pizzaAPI.exceptions.OrderNotFoundException;
 import group69.pizzaAPI.exceptions.OrderNotPlacedException;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(path = "/api/order")
 public class OrderController 
 {
